d5f88dcc66904942ab9a9422a1f3fc7de812d0a1
danielfsokol <danielfsokol@gmail.com>
2017-12-19 10:49:26 -0500

    add componentwillreceiveprops

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -10,21 +10,37 @@ class MessageList extends Component {
     this.messagesRef = this.props.firebase.database().ref('messages');
   }
 
-  componentDidMount = () => {
-    this.messagesRef.on('child_added', snapshot => {
-      const message = snapshot.val();
-      message.key = snapshot.key;
-      this.setState({ messages: this.state.messages.concat ( message ) });
-    });
+  componentWillReceiveProps = (nextProps) => {
+    if(this.props.activeRoom !== nextProps.activeRoom){
+
+      this.messagesRef.orderByChild("roomId").equalTo(nextProps.activeRoom).on('child_added', snapshot => {
+        const message = snapshot.val();
+        message.key = snapshot.key;
+        if (!this.state.messages.find((m) => m.key === message.key)){
+          this.setState({ messages: this.state.messages.concat ( message ) });
+        }
+      });
+    }
   }
+
+  // componentDidMount = () => {
+  //   this.messagesRef.on('child_added', snapshot => {
+  //     const message = snapshot.val();
+  //     message.key = snapshot.key
+  //     console.log("message: " + message.key);
+  //     this.setState({ messages: this.state.messages.concat ( message ) });
+  //     console.log(this.state.messages);
+  //   });
+  // }
 
   render() {
     return(
+
       this.state.messages.map((message, index) =>
 
         <p key={index}>{message.roomId===this.props.activeRoom ? (`${message.username}: ${message.content}`) : ""}</p>
 
-    )
+      )
     );
   }
 }
